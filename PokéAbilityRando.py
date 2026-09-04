7d0584f815e3f067aa8ce61c5a4d803016cbf2c1
Zenshu <joshua.l.miles@gmail.com>
2026-09-03 20:02:56 -0400

    Update PokéAbilityRando.py
    bumped to 1.0.7 for testing

--- PokéAbilityRando.py
+++ PokéAbilityRando.py
@@ -11,7 +11,7 @@ import requests
 from PIL import Image, ImageTk
 
 APP_NAME = "PokéAbilityRando"
-CURRENT_VERSION = "v1.0.4"
+CURRENT_VERSION = "v1.0.7"
 
 # --- Path Helpers ---
 def get_app_install_dir() -> str:
